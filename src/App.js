a1667aad8de670837fb83c38cae4e8fc12ecdff0
Tim Mickle <TimothyRMickle@gmail.com>
2018-07-31 21:34:16 -0500

    messing with pokemon api

--- src/App.js
+++ src/App.js
@@ -13,6 +13,26 @@ class App extends Component {
     }
   }
 
+  getPokemon = async() => {
+    try {
+    const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/');
+    const pokemonJson = await pokemon.json();
+    const actualPokemon = [];
+    console.log(pokemonJson)
+    
+    for( let i = 0; i< pokemonJson.length; i++) {
+      // console.log(element.url)
+      const singlePokemon =  await fetch(pokemonJson.results[i].url);
+      console.log(singlePokemon)
+      // const singlePokemonJson = await singlePokemon.json();
+      // actualPokemon.push(singlePokemon);
+    }
+    return actualPokemon
+    } catch (err) {
+        return err
+    }
+  }
+
   userHandler = (e) => {
     console.log(e.target.value)
     this.setState({
@@ -25,6 +45,12 @@ class App extends Component {
     console.log('test');
     this.setState({
       login: true
+    })
+  }
+
+  componentDidMount() {
+    this.getPokemon().then((data)=>{
+      console.log(data)
     })
   }
 
